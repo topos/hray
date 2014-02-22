8c02030760fc7316e9539d58f4e19b8425641577
ml <slacket@gmail.com>
2014-02-22 04:14:48 -0500

    bug fix

--- src/Actor/gossima.hs
+++ src/Actor/gossima.hs
@@ -9,13 +9,7 @@ import Control.Distributed.Process.Node
 import Control.Concurrent (threadDelay)
 import Network.Transport.TCP
 
-start :: Process ()
-start = do
-  sPing <- spawnChannelLocal server -- start server
-  spawnLocal $ client sPing -- start client
-  liftIO $ threadDelay 100000 -- wait a while
-
-data Ping = Ping deriving (Typeable) -- serializable (= Binary + Typeable)
+data Ping = Ping deriving (Typeable)
 
 instance Binary Ping where
   put Ping = putWord8 0
@@ -24,18 +18,23 @@ instance Binary Ping where
 server :: ReceivePort Ping -> Process ()
 server rPing = do
   Ping <- receiveChan rPing
-  liftIO $ print "ping"
+  liftIO $ putStrLn "ping"
 
 client :: SendPort Ping -> Process ()
-client sPing = do
-  sendChan sPing Ping
-  
+client sPing = sendChan sPing Ping
+
 -- main = defaultMain
 defaultMain :: IO ()
 defaultMain = do
   Right transport <- createTransport "127.0.0.1" "8080" defaultTCPParameters
   node <- newLocalNode transport initRemoteTable
   runProcess node start
+
+start :: Process ()
+start = do
+  sPing <- spawnChannelLocal server -- start server
+  spawnLocal $ client sPing -- start client
+  liftIO $ threadDelay 100000 -- wait a while
 
 -- data SendPort a     -- Serializable
 -- data ReceivePort a  -- NOT Serializable
